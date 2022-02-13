75c5b86ba7b0b304b4a829ff43cbef26b0c4217e
Eric Skelton <=>
2022-02-13 13:11:36 -0800

    more informative test

--- TransactionServer/api_app/utils/quoteServer.py
+++ TransactionServer/api_app/utils/quoteServer.py
@@ -3,8 +3,13 @@ import socket
 def getQuote(ticker):
     HOST = '192.168.4.2'
     PORT = 4444
+    print('Connecting to server at {}:{}'.format(HOST, PORT))
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((HOST, PORT))
+        try:
+            sock.connect((HOST, PORT))
+        except socket.error as err:
+            print('Error connecting to server: {}'.format(err))
+        
         sock.sendall(ticker.encode())
         data = sock.recv(1024)
     print('Received', repr(data))
