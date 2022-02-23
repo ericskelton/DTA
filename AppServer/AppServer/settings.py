7dfc05b033632ad1073aa1f824c0724829476d71
Jasperhale999 <49253531+Jasperhale999@users.noreply.github.com>
2022-02-22 20:47:16 -0400

    new modules for deploying to heroku

--- AppServer/AppServer/settings.py
+++ AppServer/AppServer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import environ
 
@@ -142,3 +142,4 @@ REST_FRAMEWORK = {
         'AppServer.apps.authentication.backends.JWTAuthentication',
     ),
 }
+django_heroku.settings(locals())
