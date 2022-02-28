219749b8b2f0ac19d488fdd18c5637a2b9d38ab7
Eric Skelton <eric.skelton00@gmail.com>
2022-02-27 18:36:57 -0800

    cron fix

--- TransactionServer/api/cron/cron.py
+++ TransactionServer/api/cron/cron.py
@@ -4,23 +4,20 @@ from api.utils.db import logJsonObject
 import time
 def trigger_job():
     triggers = getTriggers()
-    sell_triggers = triggers.get('sell_triggers', {})
-    buy_triggers = triggers.get('buy_triggers', {})
     transactionId = logJsonObject({'type': 'systemEvent', 'event': 'trigger_job', 'server': 'transactionserver', 'timestamp': str(int(time.time()))})
     startTime = time.time()
     triggers_executed = 0
-    for stock in buy_triggers:
+    for stock in triggers:
         quote = getQuote(stock)
-        if quote['price'] <= buy_triggers[stock]['price']:
-            buyStock(buy_triggers[stock]['userid'], buy_triggers[stock]['amount'], transactionId)
-            commitBuy(buy_triggers[stock]['userid'], stock, transactionId)
+        if triggers[stock]['type'] == 'buy' and quote['price'] <= triggers[stock]['price']: 
+            buyStock(triggers[stock]['userid'], triggers[stock]['amount'], transactionId)
+            commitBuy(triggers[stock]['userid'], stock, transactionId)
             triggers_executed += 1
-    for stock in sell_triggers:
-        quote = getQuote(stock)
-        if quote['price'] >= sell_triggers[stock]['price']:
+        elif triggers[stock]['type'] == 'sell' and quote['price'] >= triggers[stock]['price']: 
             sellStock(sell_triggers[stock]['userid'], sell_triggers[stock]['amount'], transactionId)
             commitSell(sell_triggers[stock]['userid'], stock, transactionId)
             triggers_executed += 1
+            
 
     logJsonObject({'type': 'systemevent', 'event': 'trigger_job_executed', 'activeTriggers': str(len(buy_triggers.keys()) + len(sell_triggers.keys())), 'triggers_executed': triggers_executed, 'time': str(time.time() - startTime) + ' seconds', 'transactionId': transactionId})
 
