2b3c847bab241e021f3f20859b362795a464baae
Eric Skelton <eric.skelton00@gmail.com>
2022-03-14 23:31:37 -0700

    fix

--- TransactionServer/api/utils/user.py
+++ TransactionServer/api/utils/user.py
@@ -346,7 +346,7 @@ def dumplogXML(username = None):
     transactionids = []
     for doc in docs:
         new_docs += '\t<'+doc['type']+'>\n'
-        if doc['type'] == 'debugEvent' and (doc.get('action', None)[:3] == 'ADD' or doc.get('COMMAND')[:3] == 'ADD') and (doc.get('transactionId', None)) :
+        if doc['type'] == 'debugEvent'  and (doc.get('transactionId', None)) :
             new_docs += '\t\t<transactionNum>'+str(doc['transactionId'])+'</transactionNum>\n'
         
         # go through the keys and add them to the xml
